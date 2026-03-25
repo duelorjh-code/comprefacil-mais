cc8ea59cfd417392d2dc69d073a8dfba068de437
Marcelo Moura <duelorjh@gmail.com>
2026-03-25 14:20:05 -0300

    Supabase-server

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -8,9 +8,9 @@ export function createSupabaseServer() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name)          { return cookieStore.get(name)?.value },
-        set(name, val, opt){ try { cookieStore.set({ name, value: val, ...opt }) } catch {} },
-        remove(name, opt)  { try { cookieStore.set({ name, value: '', ...opt }) } catch {} },
+        get(name: string)                        { return cookieStore.get(name)?.value },
+        set(name: string, val: string, opt: any) { try { cookieStore.set({ name, value: val, ...opt }) } catch {} },
+        remove(name: string, opt: any)           { try { cookieStore.set({ name, value: '', ...opt }) } catch {} },
       },
     }
   )
